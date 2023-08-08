f48e7d74391e9071c011ff5166c6aa084916dcb4
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2023-08-07 20:05:23 -0700

    syntax adjustments for deployment

--- pages/categories.js
+++ pages/categories.js
@@ -178,41 +178,43 @@ function Categories({swal}) {
                 <div class="flex flex-col">
                 <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                    <div class="overflow-hidden">
-                        <table class="min-w-full text-left text-sm font-light">
-                        <thead class="border-b font-medium dark:border-neutral-500">
-                            <tr>
-                            <th scope="col" class="px-6 py-4">Category</th>
-                            <th scope="col" class="px-6 py-4">Parent Category</th>
-                            <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {categories.length > 0 && categories.map(category => (
-                            <tr class="border-b dark:border-neutral-500">
-                            <td class="whitespace-nowrap px-6 py-4 font-medium">{category.name}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{category?.parent?.name}</td>
-                            <td>
-                                <button 
-                                onClick={() => editCategory(category)}
-                                className="btn-primary mr-2">Edit</button>
-                                <button
-                                onClick={() => deleteCategory(category)}
-                                className="btn-primary">Delete</button>
-                            </td>
-                            </tr>
-                            ))};
-                        </tbody>
-                        </table>
-                    </div>
+                        <div class="overflow-hidden">
+                            <table class="min-w-full text-left text-sm font-light">
+                                <thead class="border-b font-medium dark:border-neutral-500">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-4">Category</th>
+                                        <th scope="col" class="px-6 py-4">Parent Category</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {categories.length > 0 && categories.map(category => (
+                                        <tr key={category._id} class="border-b dark:border-neutral-500">
+                                            <td class="whitespace-nowrap px-6 py-4 font-medium">{category.name}</td>
+                                            <td class="whitespace-nowrap px-6 py-4">{category?.parent?.name}</td>
+                                            <td>
+                                                <button 
+                                                    onClick={() => editCategory(category)}
+                                                    className="btn-primary mr-2">Edit</button>
+                                                <button
+                                                    onClick={() => deleteCategory(category)}
+                                                    className="btn-primary">Delete</button>
+                                            </td>
+                                        </tr>
+                                    ))};
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                </div>
-            )}
-        </Layout>
-    );
+            </div>
+        )}
+    </Layout>
+);
 }
 
-export default withSwal(({swal}, ref) => (
-    <Categories swal={swal} />
+export default withSwal(({ swal }, ref) => (
+<Categories swal={swal} />
 ));
+
+                
--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -6,36 +6,43 @@ import axios from "axios";
 export default function DeleteProductPage() {
     const router = useRouter();
     const [productInfo, setProductInfo] = useState();
-    const {id} = router.query;
+    const { id } = router.query;
+    
     useEffect(() => {
-        if (!id){
+        if (!id) {
             return;
         }
-        axios.get("/api/products?id="+id).then(response => {
+        axios.get("/api/products?id=" + id).then(response => {
             setProductInfo(response.data);
         });
     }, [id]);
+
     function goBack() {
         router.push("/products");
     }
+
     async function deleteProduct() {
-        await axios.delete("/api/products?id="+id);
+        await axios.delete("/api/products?id=" + id);
         goBack();
     }
+
     return (
         <Layout>
             <h1 className="text-center">
                 Do you really want to delete
-                &nbsp; "{productInfo?.title}"?
+                &nbsp; &quot;{productInfo?.title}&quot;?
             </h1>
             <div className="flex gap-2 justify-center">
                 <button 
-                onClick={deleteProduct}
-                className="btn-red">Yes</button>
-            <button 
-            className="btn-default"
-            onClick={goBack}>
-            NO</button>
+                    onClick={deleteProduct}
+                    className="btn-red">
+                    Yes
+                </button>
+                <button 
+                    className="btn-default"
+                    onClick={goBack}>
+                    NO
+                </button>
             </div>
         </Layout>
     );
