4ebc9f4dca8a75e422e135ac8d283df8d6d45641
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2023-08-07 20:59:25 -0700

    added secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,33 +1,31 @@
-import NextAuth, {getServerSession} from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth, { getServerSession } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '@/lib/mongodb';
+import crypto from 'crypto';
 
 const adminEmails = [process.env.ADMIN_EMAIL];
 
+const secret = crypto.randomBytes(64).toString('hex');
+
 const authOptions = {
+  secret: secret,
   providers: [
     GoogleProvider({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-  }),
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: ({session, token, user}) => {
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    },
+    // ...
   },
 };
 
 export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
+  const session = await getServerSession({ req, res }, authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
     throw "Unauthorized request";
   }
