8f4b1422057c02e3c57a462d3c599366fb5d73d2
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2023-08-07 20:17:57 -0700

    added key values to iterators

--- pages/categories.js
+++ pages/categories.js
@@ -118,7 +118,7 @@ function Categories({swal}) {
                     <option value="">No parent category</option>
                     {categories.length > 0 && categories.map
                     (category => (
-                        <option value={category._id}>
+                        <option key={category._id} value={category._id}>
                             {category.name}
                         </option>
                     ))}
@@ -133,7 +133,7 @@ function Categories({swal}) {
                         Add new property
                     </button>
                     {properties.length > 0 && properties.map((property, index) => (
-                        <div className="flex gap-1 mb-2">
+                        <div key={index} className="flex gap-1 mb-2">
                             <input type="text"
                                         value={property.name}
                                         className="mb-0"
